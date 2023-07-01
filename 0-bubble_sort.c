3c3ded262a41c3a1ff44e3692004c9d2ef606cbe
deanbirnie <deanicdl@gmail.com>
2023-07-01 12:16:20 -0700

    Task 0 - Add check for Null array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,11 @@ void bubble_sort(int *array, size_t size)
 	size_t j;
 	int temp;
 
+	if (array == NULL)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
